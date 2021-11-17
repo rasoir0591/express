964f9ee9192a0b26d03cd775efc7474240532345
rasoir0591 <radoslaw0591@gmail.com>
2021-11-17 17:35:28 +0100

    menu item

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('admin', { title: 'Admin' });
 });
 
 module.exports = router;
--- routes/quiz.js
+++ routes/quiz.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('Quiz', { title: 'Quiz' });
 });
 
 module.exports = router;

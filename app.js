bd159abb6ff757533a6891a04010095988aae851
rasoir0591 <radoslaw0591@gmail.com>
2021-11-18 22:12:58 +0100

    api news

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require("./routes/index");
 var quizRouter = require("./routes/quiz");
 var adminRouter = require("./routes/admin");
 var newsRouter = require("./routes/news");
+var apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -44,6 +45,7 @@ app.use("/", indexRouter);
 app.use("/news", newsRouter);
 app.use("/quiz", quizRouter);
 app.use("/admin", adminRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

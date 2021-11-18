de2eca2d1f51792c5d490bc97fd77e086ad51816
rasoir0591 <radoslaw0591@gmail.com>
2021-11-18 18:54:43 +0100

    news filter articles

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const News = require("../models/news");
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+router.get("/", (req, res) => {
+  const search = req.query.search;
+
+
+  const findNews = News
+  .find({title: new RegExp(search?.trim(), 'i')})
+  .sort({created: -1});
+
+  findNews.exec((err, data) => {
+    res.render("news", { title: "News", data, search });
+  });
 });
 
 module.exports = router;

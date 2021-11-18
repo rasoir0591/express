befedbd986923b47d135a38d4b184c297b29eab3
rasoir0591 <radoslaw0591@gmail.com>
2021-11-18 21:27:19 +0100

    fix: refactor hard coded connection string to Heroku config var

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+const uri = process.env.MONGODB_URI;
 module.exports = {
+<<<<<<< HEAD
   db: "",
+=======
+  db: uri,
+>>>>>>> 16c2875 (fix: refactor hard coded connection string to Heroku config var)
   keySession: ["TWOJKLUCZ", "TWOJKLUCZ2"],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
